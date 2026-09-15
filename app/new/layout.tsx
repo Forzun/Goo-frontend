fe30f1d87203efc045fbb0bd06fae3878b0e838d
Forzun <vbhavesh219@gmail.com>
2026-09-15 09:53:15 +0530

    creating local browser

---
+++ app/new/layout.tsx
@@ -0,0 +1,45 @@
+import { Geist, Geist_Mono, VT323, Cormorant } from "next/font/google"
+
+import "./globals.css"
+import { cn } from "@/lib/utils"
+
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+const vt323 = VT323({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-vt323",
+})
+
+const cormorant = Cormorant({
+  subsets: ["latin"],
+  variable: "--font-cormorant",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        geist.variable,
+        vt323.variable,
+        cormorant.variable
+      )}
+    >
+      <body suppressHydrationWarning>{children}</body>
+    </html>
+  )
+}
